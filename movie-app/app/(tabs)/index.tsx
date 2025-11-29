98c2866f997c7f48cb340c1662ebf82a96a08bd6
DESKTOP-GVPUM2G\Admin <kris.nguyen@developertools.com>
2025-11-29 21:15:17 +0700

    Refactor layout to include tab and movie screens; remove unused index file and delete obsolete image assets

---
+++ movie-app/app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="p-4">
+      <Text className="font-bold text-5xl text-center">Welcome!</Text>
+      <Link
+        className="mt-10 underline text-blue-500"
+        href={{
+          pathname: "/movies/[id]",
+          params: { id: "1" },
+        }}
+      >
+        Avengers movie
+      </Link>
+    </View>
+  );
+}
